725529cbf51aa14ba934303711996ad7872626f1
Chris Haas <cwhaas98@gmail.com>
2021-01-16 23:11:52 -1000

    Slowed animation

--- assets/js/three.js
+++ assets/js/three.js
@@ -708,7 +708,7 @@ fragmentShader:"precision mediump float;\nuniform lowp int renderType;\nuniform 
 fragmentShader:"uniform vec3 color;\nuniform sampler2D map;\nuniform float opacity;\nuniform int fogType;\nuniform vec3 fogColor;\nuniform float fogDensity;\nuniform float fogNear;\nuniform float fogFar;\nuniform float alphaTest;\nvarying vec2 vUV;\nvoid main() {\nvec4 texture = texture2D( map, vUV );\nif ( texture.a < alphaTest ) discard;\ngl_FragColor = vec4( color * texture.xyz, texture.a * opacity );\nif ( fogType > 0 ) {\nfloat depth = gl_FragCoord.z / gl_FragCoord.w;\nfloat fogFactor = 0.0;\nif ( fogType == 1 ) {\nfogFactor = smoothstep( fogNear, fogFar, depth );\n} else {\nconst float LOG2 = 1.442695;\nfloat fogFactor = exp2( - fogDensity * fogDensity * depth * depth * LOG2 );\nfogFactor = 1.0 - clamp( fogFactor, 0.0, 1.0 );\n}\ngl_FragColor = mix( gl_FragColor, vec4( fogColor, gl_FragColor.w ), fogFactor );\n}\n}"}};
 
 
-var SEPARATION = 100,
+var SEPARATION = 120,
         AMOUNTX = 70,
         AMOUNTY = 70;
  
@@ -731,7 +731,7 @@ var SEPARATION = 100,
         container = document.querySelector('#animation');
         
  
-        camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
         camera.position.z = 1200;
  
         scene = new THREE.Scene();
@@ -852,8 +852,8 @@ var SEPARATION = 100,
             for (var iy = 0; iy < AMOUNTY; iy++) {
  
                 particle = particles[i++];
-                particle.position.y = (Math.sin((ix + count) * 0.3) * 50) + (Math.sin((iy + count) * 0.5) * 50);
-                particle.scale.x = particle.scale.y = (Math.sin((ix + count) * 0.3) + 1) * 2 + (Math.sin((iy + count) * 0.5) + 1) * 2;
+                particle.position.y = (Math.sin((ix + count) * 0.3) * 50)/1.3 + (Math.sin((iy + count) * 0.5) * 50)/1.3;
+                particle.scale.x = particle.scale.y = (Math.sin((ix + count) * 0.3) + 1) * 1.7 + (Math.sin((iy + count) * 0.5) + 1) * 1.7;
  
             }
  
@@ -861,6 +861,6 @@ var SEPARATION = 100,
  
         renderer.render(scene, camera);
  
-        count += 0.1;
+        count += 0.04;
  
     }
